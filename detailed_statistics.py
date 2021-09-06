e71996744c2b7b02ed3c54005f94fe5d60b56bf5
didiergaona <didiergaona@wposs.com>
2021-09-06 13:31:30 -0500

    contando errores e inconclusas

--- detailed_statistics.py
+++ detailed_statistics.py
@@ -23,17 +23,11 @@ LIST_REQUEST = [
     'registrarPago'
 ]
 STATE_TAG = 'estado'
-TOTAL_TAG = 'total'
-APPROVED_TAG = 'aprobadas'
 ERRORS_TAG = 'errores'
 INCONCLUSIVE_TAG = 'inconclusas'
-REQUEST_STATE = [TOTAL_TAG,
-                 APPROVED_TAG,
-                 ERRORS_TAG,
+REQUEST_STATE = [ERRORS_TAG,
                  INCONCLUSIVE_TAG]
 SEARCH_REGEX = {
-    TOTAL_TAG: r'%s.*(?:{"Timestamp"|{"Data").*',
-    APPROVED_TAG: r'%s.*(?:"codError":0|{"Codigo":"00").*',
     ERRORS_TAG: r'%s.*("codError":100|Index was out of range|Sequence contains no elements|En este momento el servicio no está disponible).*',
     INCONCLUSIVE_TAG: r'.*%s.*"Codigo":"01"(?!.*En este momento el servicio no está disponible).*'
 }
@@ -84,26 +78,25 @@ def decompress_logs(path):
 
 def monitoring():
     dict_total_request = dict.fromkeys(LIST_REQUEST)
+    dataframe = pandas.DataFrame()
     for folder in search_file(f'{LOG_PATH_FOLDER}'):
         for log in search_file(folder):
             file_log = open(log, encoding='utf-8').read()
             for request in LIST_REQUEST:
                 if dict_total_request[request] is None:
                     dict_total_request[request] = {
-                        TOTAL_TAG: 0,
-                        APPROVED_TAG: 0,
                         ERRORS_TAG: 0,
                         INCONCLUSIVE_TAG: 0
                     }
                 list_requests = re.findall(r'%s..{.*' % request, file_log)
                 string_requests = '\n'.join(list_requests)
                 for key, value in SEARCH_REGEX.items():
-                    request_list = re.findall(value % request, string_requests)
-                    df = pandas.DataFrame(data=request_list)
-                    df = df.value_counts()
-                    df.to_csv('logs_detallados.csv')
-                    requests_found = len(request_list)
+                    requests_list = re.findall(value % request, string_requests)
+                    dataframe = dataframe.append(requests_list)
+                    requests_found = len(requests_list)
                     dict_total_request[request][key] += requests_found
+    dataframe = dataframe.value_counts()
+    dataframe.to_csv('logs_detallados.csv')
     export_to_dataframe(dict_total_request)
 
 

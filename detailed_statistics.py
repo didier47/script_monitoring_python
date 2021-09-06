e478781dd70637b1eae671ee9714cd953e4067ab
didiergaona <didiergaona@wposs.com>
2021-09-06 17:03:51 -0500

    refactoring semantico

--- detailed_statistics.py
+++ detailed_statistics.py
@@ -10,7 +10,7 @@ SEARCH_REGEX = {
     ERRORS_TAG: r'%s.*("codError":100|Index was out of range|Sequence contains no elements|En este momento el servicio no está disponible).*',
     INCONCLUSIVE_TAG: r'.*%s.*"Codigo":"01"(?!.*En este momento el servicio no está disponible).*"Mensaje":(".*")'
 }
-EXPORT_FILE_NAME = 'logs_detallados'
+EXPORT_FILE_NAME = 'logs_errores_detallados'
 
 
 def export_to_dataframe(dict_total_request):
@@ -26,10 +26,10 @@ def monitoring():
         for log in search_file(folder):
             file_log = open(log, encoding='utf-8').read()
             for request in LIST_REQUEST:
-                if dict_total_request[request] is None:
-                    dict_total_request[request] = []
                 list_requests = re.findall(r'%s..{.*' % request, file_log)
                 string_requests = '\n'.join(list_requests)
+                if dict_total_request[request] is None:
+                    dict_total_request[request] = []
                 for key, value in SEARCH_REGEX.items():
                     requests_list = re.findall(value % request, string_requests)
                     dict_total_request[request] += requests_list
--- general_statistics.py
+++ general_statistics.py
@@ -14,7 +14,7 @@ SEARCH_REGEX = {
     ERRORS_TAG: r'%s.*(?:"codError":100|Index was out of range|Sequence contains no elements|En este momento el servicio no está disponible).*',
     INCONCLUSIVE_TAG: r'.*%s.*"Codigo":"01"(?!.*En este momento el servicio no está disponible).*'
 }
-EXPORT_FILE_NAME = 'logs_estado_cmb'
+EXPORT_FILE_NAME = 'logs_estado_peticiones'
 
 
 def export_to_dataframe(dict_total_request):
@@ -30,6 +30,8 @@ def monitoring():
         for log in search_file(folder):
             file_log = open(log, encoding='utf-8').read()
             for request in LIST_REQUEST:
+                list_requests = re.findall(r'%s..{.*' % request, file_log)
+                string_requests = '\n'.join(list_requests)
                 if dict_total_request[request] is None:
                     dict_total_request[request] = {
                         TOTAL_TAG: 0,
@@ -37,8 +39,6 @@ def monitoring():
                         ERRORS_TAG: 0,
                         INCONCLUSIVE_TAG: 0
                     }
-                list_requests = re.findall(r'%s..{.*' % request, file_log)
-                string_requests = '\n'.join(list_requests)
                 for key, value in SEARCH_REGEX.items():
                     requests_found = len(re.findall(value % request, string_requests))
                     dict_total_request[request][key] += requests_found

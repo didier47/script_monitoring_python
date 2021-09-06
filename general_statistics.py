212c092b7d039f029b0caef1cd86477bc99f10d4
didiergaona <didiergaona@wposs.com>
2021-09-06 16:54:05 -0500

    creado common con dependencias comunes

--- general_statistics.py
+++ general_statistics.py
@@ -1,32 +1,10 @@
-import gzip
-import os
 import re
-import shutil
-import tarfile as tar
-from glob import glob
 
 import pandas
 
-GZ_EXTENSION = 'gz'
-TAR_EXTENSION = 'tar'
-COMPRESSED_EXTENSION = [TAR_EXTENSION, GZ_EXTENSION]
-LOG_EXTENSION = 'log'
-LOG_PATH_FOLDER = r'.\LOGS_CMB'
-ALL_FILES_CHARACTERS = '/*'
-LIST_REQUEST = [
-    'validarTarjetaVirtual',
-    'iniciarSesion',
-    'obtenerServicios',
-    'obtenerCompanias',
-    'obtenerPagoServicios',
-    'obtenerCuentas',
-    'registrarPago'
-]
-STATE_TAG = 'estado'
-TOTAL_TAG = 'total'
-APPROVED_TAG = 'aprobadas'
-ERRORS_TAG = 'errores'
-INCONCLUSIVE_TAG = 'inconclusas'
+from util.common import decompress_logs, LOG_PATH_FOLDER, STATE_TAG, LIST_REQUEST, \
+    search_file, TOTAL_TAG, APPROVED_TAG, ERRORS_TAG, INCONCLUSIVE_TAG
+
 REQUEST_STATE = [TOTAL_TAG,
                  APPROVED_TAG,
                  ERRORS_TAG,
@@ -40,46 +18,11 @@ SEARCH_REGEX = {
 EXPORT_FILE_NAME = 'logs_estado_cmb'
 
 
-def search_file(path):
-    list_files = glob(f'{path}{ALL_FILES_CHARACTERS}')
-    return list_files
-
-
-def decompress_tar_file(file_path):
-    compressed_file = tar.open(file_path)
-    compressed_file.extractall(LOG_PATH_FOLDER)
-    compressed_file.close()
-
-
-def decompress_gz_file(file_path, destination_file_path):
-    with gzip.open(file_path, 'rb') as file_in, open(destination_file_path, 'wb') as file_out:
-        shutil.copyfileobj(file_in, file_out)
-
-
-def split_file_extension(path):
-    list_path_split = path.split('.')
-    file_extension = list_path_split.pop(-1)
-    file_path_with_no_extension = '.'.join(list_path_split)
-    return file_path_with_no_extension, file_extension
-
-
 def export_to_dataframe(dict_total_request):
     data_frame_logs = pandas.DataFrame(data=dict_total_request)
     data_frame_logs.insert(0, STATE_TAG, REQUEST_STATE)
     data_frame_logs.to_excel(f'{EXPORT_FILE_NAME}.xlsx', index=False)
     data_frame_logs.to_csv(f'{EXPORT_FILE_NAME}.csv', index=False)
-
-
-def decompress_logs(path):
-    for file in search_file(path):
-        file_path_with_not_extension, file_extension = split_file_extension(file)
-        if file_extension == TAR_EXTENSION and not os.path.exists(file_path_with_not_extension):
-            decompress_tar_file(file)
-            decompress_logs(file_path_with_not_extension)
-        elif file_extension == GZ_EXTENSION:
-            decompress_gz_file(file, file_path_with_not_extension)
-        if os.path.exists(file_path_with_not_extension) or file_extension == GZ_EXTENSION:
-            os.remove(file)
 
 
 def monitoring():
